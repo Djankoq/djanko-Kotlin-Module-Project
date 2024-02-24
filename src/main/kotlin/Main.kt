7c1205123d5df31f5c2bf8796bf68ef40715181c
Djankoq <dgankomez@gmail.com>
2024-02-24 08:50:14 +0300

    Практическая работа 3 коммит 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,16 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+abstract class Menu{
+    abstract fun showPointMenu()
+    abstract fun addInput(input: Int)
+
+    fun start(){
+        while(true){
+            showPointMenu()
+            val input = readln()?.toIntOrNull()?: -1
+            addInput(input)
+        }
+    }
+}
+fun main(){
+    val archives = Archives()
+    archives.start()
 }
